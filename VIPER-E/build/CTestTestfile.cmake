cf14aa6f6a4f33c7779a0b0ed482625a1be66f04
Isaac Garon <idg11777@uga.edu>
2024-02-27 16:54:08 -0500

    added initialization functions

--- VIPER-E/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/isaacgaron/Documents/UGA/CapstoneDevelopment/Capstone-Software/VIPER-E
-# Build directory: /Users/isaacgaron/Documents/UGA/CapstoneDevelopment/Capstone-Software/VIPER-E/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("pico-sdk")
-subdirs("build")
--- VIPER-E/src/VIPER-E.c
+++ VIPER-E/src/VIPER-E.c
@@ -1,11 +1,184 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#include "sd_card.h"
+#include "ff.h"
 
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+#include "hardware/i2c.h"
+#include "hardware/rtc.h"
+#include "pico/util/datetime.h"
+
+#include "pico/binary_info.h"
+
+
+
+static int addr = 0x28; // i2c address of BNO055
+
+/*
+* Initializes BNO-055. When the BNO-055 is powered,
+* it is switched from configuration mode to IMU mode, and
+* the units are set to m/s^2 for the accelerometer.
+*/
+static void bnoReset() {
+    // switches BNO into IMU mode
+    // configuration address - 0x3D | IMU mode - 0x4
+    uint8_t config_buf[] = {(uint8_t)0x3D, (uint8_t)0x4};
+    i2c_write_blocking(i2c_default, addr, config_buf, 2, false);
+    sleep_ms(30); // takes 30ms for changes to take place
+
+    // sets the units to m/s^2 for the accelerometer
+    // unit address - 0x3B | m/s^2 units - 0x0
+    uint8_t unit_buf[] = {(uint8_t)0x3B, (uint8_t)0x0};
+    i2c_write_blocking(i2c_default, addr, unit_buf, 2, false);
+} // bnoReset
+
+/*
+* Reads the Z axis of the BNO-055. Reads the 2 bytes acceleration
+* value from the Z register and returns a single int.
+*
+* @return accel_z - current acceleration in the z axis
+*/
+int16_t bnoReadZ() {
+    uint8_t buffer[2];
+    uint8_t val = 0xC; // Z axis register
+
+    // writes to the IMU to tell it what address it wants to read from
+    i2c_write_blocking(i2c_default, addr, &val, 1, true);
+    i2c_read_blocking(i2c_default, addr, buffer, 2, false); // reads 2 bytes acceleration value
+
+    return (buffer[1] << 8 | buffer[0]); // returns a single 16 bit integer
+} // bnoReadZ
+
+/*
+* Initializes Real Time Clock (RTC). The RTC is used to time the
+* execution of the program for a reliable start/stop time.
+*
+* @param date - a datetime_t struct intializing starting time
+*/
+void initializeRTC(datetime_t date) {
+    // Start the RTC
+    rtc_init();
+    rtc_set_datetime(&date);
+} // initializeRTC
+
+/*
+* Initializes the Analog to Digital Converter (ADC).
+*   
+* @param GPIO - the GPIO number of the ADC
+* @param channel - the channel number of the ADC 
+*/
+void initalizeADC(int GPIO, int channel) {
+    adc_init();
+    // Select ADC input 0 (GPIO26)
+    adc_gpio_init(GPIO);
+    adc_select_input(channel);
+} // initializeADC
+
+/*
+* Initializes I2C communications for the BNO055 Sensor. Uses the default 
+* SDA and SCL pins on the pico (GPIO 4 (pin 6) and 5 (pin 7)) with a baud
+* rate of 400000.
+*/
+void initializeI2C() {
+    i2c_init(i2c_default, 400 * 1000);
+    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
+    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+
+    // reset the BNO055
+    bnoReset();
+} // initializeI2C
+
+/*
+* Entry point into program
+*/
 int main() {
-    stdio_init_all();
-    while (1) {
-        sleep_ms(100);
-        printf("hello VIPER-E\n");
+    // variables to access and write to file
+    FRESULT fileResult;
+    FATFS fileSystem;
+    FIL file;
+    int ret;
+    char buf[100];
+    char filename[] = "prototype.csv";
+
+    // Initialize SD card
+    if (!sd_init_driver()) {
+        printf("ERROR: Could not initialize SD card\r\n");
+        while (true);
     }
+
+    // Mount drive
+    fileResult = f_mount(&fileSystem, "0:", 1);
+    if (fileResult != FR_OK) {
+        printf("ERROR: Could not mount filesystem (%d)\r\n", fileResult);
+        while (true);
+    }
+
+    // Open file for writing ()
+    fileResult = f_open(&file, filename, FA_WRITE | FA_CREATE_ALWAYS);
+    if (fileResult != FR_OK) {
+        printf("ERROR: Could not open file (%d)\r\n", fileResult);
+        while (true);
+    }
+
+    double acc_z = 0;
+
+    char datetime_buf[256];
+    char *datetime_str = &datetime_buf[0];
+
+    // Start on Friday 5th of June 2020 15:45:00
+    datetime_t date = {
+            .year  = 2024,
+            .month = 02,
+            .day   = 20,
+            .dotw  = 2, // 0 is Sunday, so 5 is Friday
+            .hour  = 18,
+            .min   = 00,
+            .sec   = 00
+    };
+
+    // Initialize chosen serial port
+    stdio_init_all();
+
+    // Initialize periphials
+    initalizeADC(26, 0);
+    initializeI2C();
+    initializeRTC(date);
+    sleep_us(64);
+
+    // Wait for user to press 'enter' to continue
+    printf("\r\nAnalog read test. Press 'enter' to start.\r\n");
+    while (true) {
+        buf[0] = getchar();
+        if ((buf[0] == '\r') || (buf[0] == '\n')) {
+            break;
+        }
+    }
+
+    int min = date.min;
+    uint16_t result = 0;
+    const float conversion_factor = 3.3f / (1 << 12);
+    int counter = 0;
+
+    // Close file
+    fileResult = f_close(&file);
+    if (fileResult != FR_OK) {
+        printf("ERROR: Could not close file (%d)\r\n", fileResult);
+        while (true);
+    }
+
+    // Unmount drive
+    f_unmount("0:");
+
+    // // Loop forever doing nothing
+    while (true) {
+        sleep_ms(1000);
+    }
+
 }
