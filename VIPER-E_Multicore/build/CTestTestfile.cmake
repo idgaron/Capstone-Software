f3bfe8f3bf1165a04426ab10274f75257681fa1c
ZachJones7 <zachjones@Zachs-MacBook-Air.local>
2024-04-16 08:54:04 -0400

    Updating

--- VIPER-E_Multicore/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/zachjones/Desktop/Spring_2024_Classes/Capstone_2/Capstone-Software/VIPER-E_Multicore
-# Build directory: /Users/zachjones/Desktop/Spring_2024_Classes/Capstone_2/Capstone-Software/VIPER-E_Multicore/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("pico-sdk")
-subdirs("lib/no-OS-FatFS-SD-SPI-RPi-Pico/FatFs_SPI")
